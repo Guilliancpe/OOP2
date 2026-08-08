bc9ac7de1459141d3f4e1f15ee5b501d0992dee7
Guilliancpe <qgcignacio@tip.edu.ph>
2026-08-08 02:02:03 +0000

    Parent file and main file

---
+++ OOP PRELIM EXAM/Parentfile.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os, csv
+
+class Parent_file:
+    def __init__(self, filename = "Recipe.csv"):
+        self.filename = filename
+    
+    def create_file(self):
+        if not os.path.exists(self.filename):
+             with open(self.filename, "a", newline="") as my_file:
+                write = csv.writer(my_file)
+                write.writerow([ "Adobo", "Lumpia", "Sinigang", "Nilaga", 
+                                "Bicol Express", "Sisig", "Kare-Kare", " Pancit", 
+                                "Chopseuy", "Tinola"])
